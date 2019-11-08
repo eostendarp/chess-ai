a1215bf72e4dbfce1254d83897e7408fc95f8d17
tclerico <timclerico@gmail.com>
2019-11-08 14:28:30 -0500

    added display game

--- chess_game.py
+++ chess_game.py
@@ -11,14 +11,14 @@ class ChessGame:
         self.agent2 = agent2
         self.board = chess.Board()
 
-    def play_game(self):
+    def play_game(self, display_moves=False):
         '''
         runs a single game of chess between two agents
         :return: the result of the game
         '''
         end_state = {}
         while not self.board.is_game_over():
-            self.play_round()
+            self.play_round(display_move=display_moves)
         result = self.board.result()
         state = result.split("-")
         if state[0] == '1/2':
@@ -30,7 +30,12 @@ class ChessGame:
 
     def play_round(self, display_move=False):
         self.play_move(self.agent1)
+        if display_move:
+            print(str(self.board) + "\n")
+
         self.play_move(self.agent2)
+        if display_move:
+            print(str(self.board) + "\n")
 
     def play_move(self, agent):
         chosen_move = agent.get_move(copy.deepcopy(self.board))
@@ -52,10 +57,11 @@ def compare_agents(agent1, agent2, num_games, display_moves=False):
             game = ChessGame(agent1, agent2)
         else:
             game = ChessGame(agent2, agent1)
-        results = game.play_game()
+        results = game.play_game(display_moves=display_moves)
         tally[agent1.color] += results[agent1.color]
         tally[agent2.color] += results[agent2.color]
         tally['Tie'] += results['Tie']
+        print(str(game.board)+'\n')
 
     return tally
 
