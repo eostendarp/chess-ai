1e74f93a9fd068965bc43ade41296fa50e0a4e36
tclerico <timclerico@gmail.com>
2019-11-13 17:04:37 -0500

    Working alpha beta

--- agents/alpha_beta_agent.py
+++ agents/alpha_beta_agent.py
@@ -19,7 +19,7 @@ class AlphaBetaAgent(BaseAgent):
 
         for move in possible_moves:
             board.push_uci(move.uci())
-            score = self.alphabeta(board, self.heuristic, float('-inf'), float('inf'), False, current_depth + 1, self.maximum_depth)
+            score = self.alpha_beta(board, self.heuristic, float('-inf'), float('inf'), False, current_depth + 1, self.maximum_depth)
             board.pop()
 
             if score > best_score:
@@ -30,30 +30,54 @@ class AlphaBetaAgent(BaseAgent):
 
         return best_move
 
-    def alphabeta(self, board, heuristic, alpha, beta, max_turn, current_depth, maximum_depth):
+    def alpha_beta(self, board, heuristic, alpha, beta, max_turn, current_depth, maximum_depth):
 
         if current_depth == maximum_depth or board.is_game_over():
             return heuristic(board, self.color_bool)
 
         possible_moves = [move for move in board.legal_moves]
         shuffle(possible_moves)
-        if max_turn:
-            score = float('-inf')
-            for move in possible_moves:
-                board.push_uci(move.uci())
-                score = max(score, self.alphabeta(board, heuristic, alpha, beta, not max_turn, current_depth + 1, maximum_depth))
-                if score >= beta:
-                    return score
-                alpha = max(alpha, score)
 
-        else:
-            score = float('inf')
-            for move in possible_moves:
-                board.push_uci(move.uci())
-                score = min(score, self.alphabeta(board, heuristic, alpha, beta, not max_turn, current_depth + 1, maximum_depth))
-                if score <= alpha:
-                    return score
-                beta = min(beta, alpha)
+        best_score = float('-inf') if max_turn else float('inf')
+        for move in possible_moves:
+            board.push_uci(move.uci())
+            score = self.alpha_beta(board, heuristic, alpha, beta, not max_turn, current_depth+1, maximum_depth)
+            board.pop()
 
-        board.pop()
-        return score
+            if max_turn and score > best_score:
+                best_score = score
+                if best_score >= beta:
+                    return best_score
+                alpha = max(alpha, best_score)
+
+            if not max_turn and score < best_score:
+                best_score = score
+                if best_score <= alpha:
+                    return best_score
+                beta = min(beta, best_score)
+
+
+
+        return best_score
+
+
+        # if max_turn:
+        #     score = float('-inf')
+        #     for move in possible_moves:
+        #         board.push_uci(move.uci())
+        #         score = max(score, self.alpha_beta(board, heuristic, alpha, beta, not max_turn, current_depth + 1, maximum_depth))
+        #         if score >= beta:
+        #             return score
+        #         alpha = max(alpha, score)
+        #
+        # else:
+        #     score = float('inf')
+        #     for move in possible_moves:
+        #         board.push_uci(move.uci())
+        #         score = min(score, self.alpha_beta(board, heuristic, alpha, beta, not max_turn, current_depth + 1, maximum_depth))
+        #         if score <= alpha:
+        #             return score
+        #         beta = min(beta, alpha)
+        #
+        # board.pop()
+        # return score
