ed1e2a04706e76cfc37543a2b4c7f4031ce1cb75
Sean McQuillan <mcquill99@gmail.com>
2019-12-03 11:09:12 -0500

    Added PV-line

--- agents/pv_agent.py
+++ agents/pv_agent.py
@@ -2,11 +2,12 @@ from agents.base_agent import BaseAgent
 from random import shuffle
 
 
-class AlphaBetaAgent(BaseAgent):
+class PVAgent(BaseAgent):
     def __init__(self, color, heuristic, maximum_depth):
         super().__init__(color)
         self.heuristic = heuristic
         self.maximum_depth = maximum_depth
+        self.pline = []
 
     def get_move(self, board):
         current_depth = 0
@@ -14,6 +15,8 @@ class AlphaBetaAgent(BaseAgent):
         shuffle(possible_moves)
         best_move = None
         best_score = float('-inf')
+        line = []
+        score_array = [best_score]
 
         for move in possible_moves:
             board.push_uci(move.uci())
@@ -21,7 +24,9 @@ class AlphaBetaAgent(BaseAgent):
             if board.is_checkmate() and board.turn != self.color:
                 return move
 
-            score = self.alpha_beta(board, self.heuristic, float('-inf'), float('inf'), False, current_depth + 1, self.maximum_depth)
+            score = self.alpha_beta(board, self.heuristic, float('-inf'), float('inf'), False, current_depth + 1,
+                                    self.maximum_depth, score_array, line)
+
             board.pop()
 
             if score > best_score:
@@ -30,14 +35,25 @@ class AlphaBetaAgent(BaseAgent):
                 best_score = score
                 best_move = move
 
+        line.reverse()
         print("AlphaBeta:", best_score)
         print("Best Move:", best_move)
+        print(line)
         return best_move
 
-    def alpha_beta(self, board, heuristic, alpha, beta, max_turn, current_depth, maximum_depth):
+    def alpha_beta(self, board, heuristic, alpha, beta, max_turn, current_depth, maximum_depth, best, pline):
+        original_best = best[0]
 
         if current_depth == maximum_depth or board.is_game_over():
-            return heuristic(board, self.color, max_turn)
+            curr_score = heuristic(board, self.color, max_turn)
+
+            if curr_score > best[0]:
+                pline.clear()
+                best.clear()
+                best.append(curr_score)
+                return curr_score
+            else:
+                return best[0]
 
         possible_moves = [move for move in board.legal_moves]
         shuffle(possible_moves)
@@ -45,13 +61,20 @@ class AlphaBetaAgent(BaseAgent):
         best_score = float('-inf') if max_turn else float('inf')
         for move in possible_moves:
             board.push_uci(move.uci())
-            score = self.alpha_beta(board, heuristic, alpha, beta, not max_turn, current_depth + 1, maximum_depth)
-            board.pop()
+            score = self.alpha_beta(board, heuristic, alpha, beta, not max_turn, current_depth + 1, maximum_depth, best,
+                                    pline)
+
+            if original_best != best[0]:
+                original_best = best[0]
+                pline.append(board.pop().uci())
+            else:
+                board.pop()
 
             if max_turn and score > best_score:
                 best_score = score
                 if best_score >= beta:
                     return best_score
+
                 alpha = max(alpha, best_score)
 
             if not max_turn and score < best_score:
