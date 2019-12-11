f696c31dcf274d04f68ef54b4a9f77999a31079c
tclerico <timclerico@gmail.com>
2019-12-11 15:00:35 -0500

    AHHHHH

--- chess_game.py
+++ chess_game.py
@@ -114,7 +114,7 @@ def run():
     print("Comparing Agents")
     agent1, agent2 = [CombinedAgent(True, combined, 3, load_hh=True), CombinedAgentTrans(False, combined, 3, load_hh=True)]
     tally, avg = compare_agents(agent1, agent2, 1, True)
-    #ttu.write_trans_table(agent2.trans_table, getcwd() + '/data/alpha_beta/trans_table.pickle')
+    ttu.write_trans_table(agent2.trans_table, getcwd() + '/data/combined_agent/trans_table.pickle')
     print(tally)
     print("Average Decision Times:", avg)
 
